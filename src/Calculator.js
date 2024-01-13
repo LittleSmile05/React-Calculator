67a3a83edf8a5696bb5ece35034d33705ec14d98
Gunel Azizova <111835072+LittleSmile05@users.noreply.github.com>
2024-01-13 15:49:56 +0400

    Add files via upload

---
+++ src/Calculator.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import './Calculator.css';
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleButtonClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input).toString());
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput(input + value);
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    const key = event.key;
+
+    if (
+      (key >= '0' && key <= '9') ||
+      key === '.' ||
+      key === '/' ||
+      key === '*' ||
+      key === '-' ||
+      key === '+' ||
+      key === '='
+    ) {
+      handleButtonClick(key);
+    } else if (key === 'Enter') {
+      handleButtonClick('=');
+    } else if (key === 'Backspace') {
+      handleButtonClick('C');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [input]);
+
+  return (
+    <div className="calculator">
+      <div className="display">
+        <input type="text" value={input} readOnly />
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleButtonClick('7')}>7</button>
+        <button onClick={() => handleButtonClick('8')}>8</button>
+        <button onClick={() => handleButtonClick('9')}>9</button>
+        <button onClick={() => handleButtonClick('/')}>/</button>
+        <button onClick={() => handleButtonClick('4')}>4</button>
+        <button onClick={() => handleButtonClick('5')}>5</button>
+        <button onClick={() => handleButtonClick('6')}>6</button>
+        <button onClick={() => handleButtonClick('*')}>*</button>
+        <button onClick={() => handleButtonClick('1')}>1</button>
+        <button onClick={() => handleButtonClick('2')}>2</button>
+        <button onClick={() => handleButtonClick('3')}>3</button>
+        <button onClick={() => handleButtonClick('-')}>-</button>
+        <button onClick={() => handleButtonClick('0')}>0</button>
+        <button onClick={() => handleButtonClick('.')}>.</button>
+        <button onClick={() => handleButtonClick('=')}>=</button>
+        <button onClick={() => handleButtonClick('+')}>+</button>
+        <button style={{ background: 'red' }} onClick={() => handleButtonClick('C')}>
+          C
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
